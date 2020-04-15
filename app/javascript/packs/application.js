86ef9d12ca9dfd2fa56e64dba72768e7ab6c712c
Vladislav-Oleksienko <splinfao1@gmail.com>
2020-04-15 07:14:26 +0300

    deleted trash img, moved css code from assets to new folder src

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,4 +16,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+
+import "../src/stylesheets/application"
+import "../src/javascript/application"
 import "bootstrap"
